0e4e7d23b4e366d967ac3aaa80212245b057e2ae
Uncle88 <andreyproskura88@gmail.com>
2017-10-17 17:03:57 +0300

    linq to array

---
+++ LinqToObjects/LinqToObjects/LinqToArray.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace LinqToObjects
+{
+    public class LinqToArray
+    {
+        public static string[] favouriteCars = { "Skoda Yetti", "Wolksvagen Golf7", "Seat Ibiza", "Reno Scenic", "Skoda Fabia", "Hundai i30", "Wolksvagen Polo"};
+    }
+}
---
+++ LinqToObjects/LinqToObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqToObjects
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IEnumerable<string> someCars = from item in LinqToArray.favouriteCars where item.Contains("7") orderby item select item;
+
+            foreach (var item in someCars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
