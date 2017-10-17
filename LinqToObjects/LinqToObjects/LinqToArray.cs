b4956b8d3085ff8e7cc2913e82450e75af6dc3e0
Uncle88 <andreyproskura88@gmail.com>
2017-10-17 17:25:36 +0300

    deferred execution linq

--- LinqToObjects/LinqToObjects/LinqToArray.cs
+++ LinqToObjects/LinqToObjects/LinqToArray.cs
@@ -3,6 +3,6 @@ namespace LinqToObjects
 {
     public class LinqToArray
     {
-        public static string[] favouriteCars = { "Skoda Yetti", "Wolksvagen Golf7", "Seat Ibiza", "Reno Scenic", "Skoda Fabia", "Hundai i30", "Wolksvagen Polo"};
+        public static string[] favouriteCars = { "Skoda Yetti", "Wolksvagen Golf7", "Seat Ibiza", "Reno Scenic", "Skoda Fabia1.4", "Hundai i30", "Wolksvagen Polo1.4"};
     }
 }
--- LinqToObjects/LinqToObjects/Program.cs
+++ LinqToObjects/LinqToObjects/Program.cs
@@ -10,10 +10,23 @@ namespace LinqToObjects
         {
             IEnumerable<string> someCars = from item in LinqToArray.favouriteCars where item.Contains("7") orderby item select item;
 
+            Console.WriteLine("*******first array\n");
             foreach (var item in someCars)
             {
                 Console.WriteLine(item);
             }
+
+            LinqToArray.favouriteCars[0] = "Volvo";
+
+            IEnumerable<string> newSomeCar = from item in LinqToArray.favouriteCars where item.Contains(".") 
+                                                                         where item.Contains("") orderby item select item;
+
+            Console.WriteLine("\n*******Array after change\n");
+            foreach (var item in newSomeCar)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
